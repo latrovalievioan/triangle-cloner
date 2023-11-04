a3c2a7098dc35a2b374c0637861c96be22d964c7
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-11-05 00:55:13 +0300

    add jsdom and test render function

---
+++ Cloner/render.test.ts
@@ -0,0 +1,57 @@
+import { describe, test, expect, beforeEach } from "vitest";
+import Cloner from "./index";
+import { JSDOM } from "jsdom";
+
+describe("render", () => {
+  let dom: JSDOM;
+  let container: HTMLElement;
+
+  beforeEach(() => {
+    dom = new JSDOM(`<div id="root"></div>`);
+    container = dom.window.document.getElementById("root") as HTMLElement;
+  });
+
+  test("doesn't crash", () => {
+    const element = Cloner.createElement("ul", { id: "test" });
+
+    expect(() => Cloner.render(element, container)).not.toThrowError;
+  });
+
+  test("renders textElement correctly", () => {
+    const element = {
+      type: "TEXT_ELEMENT",
+      props: {
+        nodeValue: "hello",
+        children: [],
+      },
+    };
+
+    Cloner.render(element, container);
+
+    expect(container.firstChild?.textContent).toEqual("hello");
+  });
+
+  test("renders tree correctly", () => {
+    const h1 = Cloner.createElement("h1", { id: "h1" }, "HELLO");
+    const h2 = Cloner.createElement("h2", { id: "h2" }, "hello");
+    const div = Cloner.createElement("div", { id: "div" }, h1, h2);
+
+    Cloner.render(div, container);
+
+    expect(container.firstChild?.firstChild?.textContent).toEqual("HELLO");
+    expect(container.firstChild?.lastChild?.textContent).toStrictEqual("hello");
+  });
+
+  test("puts correct props on a rendered elements", () => {
+    const handleClick = () => alert("YEAH");
+    const div = Cloner.createElement("div", {
+      id: "div",
+      onClick: handleClick,
+    });
+
+    Cloner.render(div, container);
+
+    expect(container.firstChild.id).toEqual("div");
+    expect(container.firstChild.onClick).toEqual(handleClick);
+  });
+});
